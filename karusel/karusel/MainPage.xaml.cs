11c6448f71de7c029a8ba5d7781dea406bd17d5a
opilane <opilane@HP-CZC6297BB7>
2020-11-17 12:31:08 +0200

    pochti sdelal

--- karusel/karusel/MainPage.xaml.cs
+++ karusel/karusel/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace karusel
     {
         public MainPage()
         {
-            var redContentPage = new ContentPage
+            var start = new ContentPage
             {
                 Content = new StackLayout
                 {
@@ -20,21 +20,82 @@ namespace karusel
                     {
                         new Label
                         {
-                            Text = "Red",
+                            Text = "Стартовая страница\nВиды темперамента",
+                            TextColor = Color.LightBlue,
                             FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label))
                         },
-                        new BoxView
-                        {
-                            Color = Color.Red,
-                            WidthRequest = 200,
-                            HeightRequest = 200,
-                            HorizontalOptions = LayoutOptions.Center,
-                            VerticalOptions = LayoutOptions.CenterAndExpand
-                        }
-
                     }
                 }
             };
+            var holerik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Холерик",
+                            TextColor = Color.Green,
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label))
+                        },
+                        new Image {Source = "holerikodin.jpg"}
+                    }
+                }
+            };
+            var sangvinik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Сангвиник",
+                            TextColor = Color.Orange,
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label))
+                        },
+                        new Image {Source = "sangvinikodin.jpg"}
+                    }
+                }
+            };
+            var flegmatik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Флегматик",
+                            TextColor = Color.Purple,
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label))
+                        },
+                        new Image {Source = "flegmatikodin.jpg"}
+                    }
+                }
+            };
+            var melanholik = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Меланхолик",
+                            TextColor = Color.Chocolate,
+                            FontSize = Device.GetNamedSize (NamedSize.Medium, typeof(Label))
+                        },
+                        new Image {Source = "melanholikodin.jpg"}
+                    }
+                }
+            };
+            Children.Add(start);
+            Children.Add(holerik);
+            Children.Add(sangvinik);
+            Children.Add(flegmatik);
+            Children.Add(melanholik);
         }
     }
 }
